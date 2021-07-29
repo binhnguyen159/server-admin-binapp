eced9d16e05bccd074cbefe3a349b03b2057820e
binhnguyen159 <ngt.binh@tego.global>
2021-07-29 21:38:17 +0700

    update cros auth-token

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,7 +4,11 @@ const Admin = require('../model/Admin');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const verifyToken = require('../middlewares/verifyToken');
-
+// router.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 router.post('/create', verifyToken, (req, res) => {
     try {
         const hash = bcrypt.hashSync(req.body.hash, parseInt(process.env.SALT));
